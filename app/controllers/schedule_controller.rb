085cc2b604adda23b495a53673adb25820f5409d
Timothy Johnson @ Found <timothy@wearefound.com>
2012-03-16 00:22:20 -0400

    schedule somewhat working

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,7 +1,7 @@
 class ScheduleController < ApplicationController
   
   def index
-    @crews = Crew.find(:all, :include => [:contracts, {:contracts => :schedule_day}])
+    @crews = Crew.find(:all)
     @contracts = Contract.where("scheduled_date > ?", Time.now - 2.days).order("scheduled_date ASC")
     @queued_contracts = Contract.where(:scheduled_date => nil)
     # @schedule_days = ScheduleDay.find(:all, :include => [:contracts]).select{|d| (Date.parse(d.day) >= Date.today && (Date.parse(d.day) <= (Date.today + 10.days)))}
