3a05bd70995e77b220ca1777f8685daff9490d24
Kevin Hopkins <kevin@wearefound.com>
2012-03-16 01:48:21 -0400

    changed routes for catchall for scheudle and removed alert

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -8,4 +8,10 @@ class ScheduleController < ApplicationController
     # @schedule_days = ScheduleDay.find(:all, :include => [:contracts]).select{|d| (Date.parse(d.day) >= Date.today && (Date.parse(d.day) <= (Date.today + 10.days)))}
   end
   
+  def get_queued_for_dropdown
+    @queued_contracts = Contract.where(:scheduled_date => nil)
+    
+    render @queued_contracts = Contract.where(:scheduled_date => nil).to_json
+  end
+  
 end
