f3006388f57f89a3a51fe2a745a8d1bcb7531a4d
Kevin Hopkins <kevin@wearefound.com>
2012-03-26 16:12:35 -0400

    changing crew

--- public/assets_old/application-e7cb4c27e9425cc2904b98ad059968df.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".fg-button").hover(function(){$(this).removeClass("ui-state-default").addClass("ui-state-focus")},function(){$(this).removeClass("ui-state-focus").addClass("ui-state-default")}),$("#hierarchy").click(function(){return!1}),$.get("/dashboard/navigation",function(a){$("#hierarchy").menu({content:a,showSpeed:400}),$("#hierarchy").removeClass("hidden")})});
