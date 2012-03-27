4e8ef70a2e72652a6b8e5effd322edf8b60d239d
Kevin Hopkins <kevin@wearefound.com>
2012-03-27 16:19:37 -0400

    added in new schedule

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'geokit-rails3'
 gem 'devise'
 gem 'mail'
 gem 'gmaps4rails'
+gem 'will_paginate'
 
 group :development do
   gem 'heroku'
--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -2,7 +2,7 @@ class ScheduleController < ApplicationController
   
   def index
     @crews = Crew.find(:all, :include => [:contracts, {:contracts => :estimate}], :order => "ordering ASC")
-    @contracts = Contract.where("scheduled_date > ?", (Time.now - 2.days)).includes(:estimate, {:estimate => :job}).order("scheduled_date ASC")
+    @contracts = Contract.where("scheduled_date > ?", (Time.now - 15.days)).includes(:estimate, {:estimate => :job}).order("scheduled_date ASC")
     @queued_contracts = Contract.where(:scheduled_date => nil).sort{|x,y| x.estimate.invoice_number <=> y.estimate.invoice_number}
   end
   
