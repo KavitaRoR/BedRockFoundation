7171eee0c07aac2ff205238e5e3dfc7eeb703636
Kevin Hopkins <kevin@wearefound.com>
2012-04-26 09:35:52 -0400

    added case for self.job

--- app/models/job_addition.rb
+++ app/models/job_addition.rb
@@ -6,6 +6,8 @@ class JobAddition < ActiveRecord::Base
   protected
   
   def update_job_price
-    self.job.update_attribute(:price_in_cents, (self.job.price_in_cents - self.addition_price_in_cents))
+    if self.job
+      self.job.update_attribute(:price_in_cents, (self.job.price_in_cents - self.addition_price_in_cents))
+    end
   end
 end
