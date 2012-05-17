0821ee9e044faf0b1e38d1dde950b399e254bd2c
Timothy Johnson @ Found <timothy@wearefound.com>
2012-05-17 07:13:57 -0400

    fix syntax

--- app/models/concrete_job_calculator.rb
+++ app/models/concrete_job_calculator.rb
@@ -149,9 +149,8 @@ class AdhocJobCalculator
     time_on_road = driving_hours.to_f
     number_of_laborers = @laborers.to_f
     laborer_rate = @laborer_rate.to_f
-    driving_labor_cost = (number_of_laborers + @job.extra_man_days.to_f) * time_on_road * laborer_rate + 
-
-    return driving_labor_cost
+    driving_labor_cost = (number_of_laborers + @job.extra_man_days.to_f) * time_on_road * laborer_rate
+    driving_labor_cost
   end
   
   def working_labor_hours
