1f61835246a61ac4b4529ffae3321135ed87b732
Courtney Jones <courtney@whitey-found.local>
2012-06-11 17:16:39 -0400

    hit two items off punchlist

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -115,7 +115,8 @@ class ScheduleController < ApplicationController
         :foundation => "<strong>Foundation:</strong> #{@options_for_job[:job_width]}' x #{@options_for_job[:job_length]}' - #{@options_for_job[:job_quality]} #{@job.foundation.kind.to_s.downcase.capitalize rescue "Shed"}", 
         :erosion_control => "#{@job.erosion_control_lft rescue 0} lft of <strong>Erosion Control Wire</strong>", 
         :offlevel => "#{(@estimate.job.off_level_amount_in_inches rescue 0) || 0} inches <strong>off level</strong>",
-        :trex_color => "#{@estimate.job.trex_color || "no"}<strong> Trex</strong>"
+        :trex_color => "#{@estimate.job.trex_color || "no"}<strong> Trex</strong>", 
+        shed_company: "<strong>Referrer: </strong>#{@estimate.job.contact.campaign.name rescue ""}"
       }
     else
       render :text => "no"
