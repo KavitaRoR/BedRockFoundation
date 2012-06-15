24a93d32ddff378b6c4e8503ce28264bbb29fafc
Timothy Johnson @ Found <timothy@wearefound.com>
2012-06-15 01:19:58 -0400

    added everyones to-do list

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -23,6 +23,24 @@ class DashboardController < ApplicationController
     end
   end
   
+  def todos 
+    @all_todos = Status.find(:all, :conditions => {:done => nil}, :include => [:contact, :job, :next_action, {:contact => :jobs}], :order => "followup_date DESC")
+    id_group = {}
+    @all_todos.each do |t|
+      if t.contact
+        if id_group.has_key?(t.contact.id)
+          id_group[t.contact.id] = t.id if id_group[t.contact.id] < t.id
+        else
+          id_group[t.contact.id] = t.id
+        end
+      end
+    end
+    @todos = []
+    id_group.each do |k, v|
+      @todos << Status.find(v, :include => [:contact, :job, :next_action, {:contact => :jobs}])
+    end
+  end
+  
   def clear_todo
     @status = Status.find(params[:id])
     @status.update_attribute "done", true
