dfacab0d9d43b3789b56d901e2de2ed69cee05e6
Kevin Hopkins <kevin@wearefound.com>
2012-06-22 15:49:43 -0400

    mail stuff

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,10 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.gmail.com',
-  :port           => '587',
-  :authentication => :login,
-  :enable_starttls_auto => true,
-  :user_name      => "apps@wearefound.com",
-  :password       => "asdqwe123qwe",
-  :domain         => 'wearefound.com'
-}
-ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   :address        => 'smtp.gmail.com',
+#   :port           => '587',
+#   :authentication => :login,
+#   :enable_starttls_auto => true,
+#   :user_name      => "apps@wearefound.com",
+#   :password       => "asdqwe123qwe",
+#   :domain         => 'wearefound.com'
+# }
+# # ActionMailer::Base.delivery_method = :smtp
