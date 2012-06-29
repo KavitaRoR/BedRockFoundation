6b2a87f9dbe862339004fa803c66181eef6fdcfc
Kevin Hopkins <kevin@wearefound.com>
2012-06-29 21:50:43 +0400

    comit from production.  removed timezone, checked database yml back out.
    not a fan of not having a time zone configured when we are being based off server time.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+# config.time_zone = "Eastern Time (US & Canada)"
 # Initialize the rails application
 Abundant::Application.initialize!
